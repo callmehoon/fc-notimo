4033f6db3b0cb27d306471a6f4d0d2dd58962870
방혜준 <kkngrs2@gmail.com>
2025-09-04 15:57:35 +0900

    refactor(PublicTemplate): 엔티티 구조 개선 및 비즈니스 메소드 추가
    - @DaTa를 제거하고 @Getter와 @Builder 애너테이션 추가
    - 기본생성자를 @NoArgsConstructor 애너테이션으로 변경
    - 필드 기본값 설정을 생성자로 이동하여 초기화 로직 통합
    - 조회수/공유수 증가 메소드(increaseViewCount, increaseShareCount) 추가
    - 데이터베이스 스키마와 Java 엔티티 간 매핑 명확화

--- backend-spring/src/main/java/com/jober/final2teamdrhong/entity/PublicTemplate.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/entity/PublicTemplate.java
@@ -5,46 +5,76 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Data
+@Entity(name = "public_template")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLDelete(sql = "UPDATE public_template SET is_deleted = true, deleted_at = NOW() WHERE public_template_id = ?")
 public class PublicTemplate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "public_template_id")
     private Integer publicTemplateId;
 
-    @Column(nullable = false, length = 255)
+    @Column(name = "public_template_title", nullable = false, length = 255)
     private String publicTemplateTitle;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(name = "public_template_content", nullable = false, columnDefinition = "TEXT")
     private String publicTemplateContent;
 
-    @Column(length = 50)
+    @Column(name = "button_title", length = 50)
     private String buttonTitle;
 
-    @Column(nullable = false)
-    private Integer shareCount = 0;
+    @Column(name = "share_count", nullable = false)
+    private Integer shareCount;
 
-    @Column(nullable = false)
-    private Integer viewCount = 0;
+    @Column(name = "view_count", nullable = false)
+    private Integer viewCount;
 
-    @Column(nullable = false)
-    private Boolean isDeleted = false;
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
 
     @CreationTimestamp
-    @Column(columnDefinition = "DATETIME", nullable = false, updatable = false)
+    @Column(name = "created_at", columnDefinition = "DATETIME", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(columnDefinition = "DATETIME")
+    @Column(name = "deleted_at", columnDefinition = "DATETIME")
     private LocalDateTime deletedAt;
 
-    protected PublicTemplate() {}
-
-    public PublicTemplate(String title, String content, Boolean isDeleted) {
-        publicTemplateTitle = title;
-        publicTemplateContent = content;
-        this.isDeleted = isDeleted;
+    /**
+     * 공개 템플릿 생성자
+     * 
+     * @param publicTemplateTitle 템플릿 제목
+     * @param publicTemplateContent 템플릿 내용
+     * @param buttonTitle 버튼 제목 (선택사항)
+     */
+    @Builder
+    public PublicTemplate(String publicTemplateTitle, String publicTemplateContent, String buttonTitle) {
+        this.publicTemplateTitle = publicTemplateTitle;
+        this.publicTemplateContent = publicTemplateContent;
+        this.buttonTitle = buttonTitle;
+        this.viewCount = 0;
+        this.shareCount = 0;
+        this.isDeleted = false;
+    }
+    
+    /**
+     * 조회수를 1 증가시킨다.
+     * 템플릿이 조회될 때마다 호출되어야 한다.
+     */
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+    
+    /**
+     * 공유수를 1 증가시킨다.
+     * 템플릿이 '가져오기'될 때마다 호출되어야 한다.
+     */
+    public void increaseShareCount() {
+        this.shareCount++;
     }
 }
