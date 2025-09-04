211fec8a5770d9c94f68febfe69f7ca34a327d39
방혜준 <kkngrs2@gmail.com>
2025-09-04 12:38:58 +0900

    refactor(PublicTemplate): 엔티티 구조 개선 및 비즈니스 메소드 추가
    - @Data를 제거하고 @Getter와 @Builder 애너테이션 추가
    - 기본생성자를 @NoArgsConstructor 애너테이션으로 변경
    - 필드 기본값 설정을 생성자로 이동하여 초기화 로직 통합
    - 조회수/공유수 증가 메소드(increaseViewCount, increaseShareCount) 추가

--- backend-spring/src/main/java/com/jober/final2teamdrhong/entity/PublicTemplate.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/entity/PublicTemplate.java
@@ -5,10 +5,14 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLDelete(sql = "UPDATE public_template SET is_deleted = true, deleted_at = NOW() WHERE public_template_id = ?")
 public class PublicTemplate {
     @Id
@@ -25,13 +29,13 @@ public class PublicTemplate {
     private String buttonTitle;
 
     @Column(nullable = false)
-    private Integer shareCount = 0;
+    private Integer shareCount;
 
     @Column(nullable = false)
-    private Integer viewCount = 0;
+    private Integer viewCount;
 
     @Column(nullable = false)
-    private Boolean isDeleted = false;
+    private Boolean isDeleted;
 
     @CreationTimestamp
     @Column(columnDefinition = "DATETIME", nullable = false, updatable = false)
@@ -40,11 +44,36 @@ public class PublicTemplate {
     @Column(columnDefinition = "DATETIME")
     private LocalDateTime deletedAt;
 
-    protected PublicTemplate() {}
-
-    public PublicTemplate(String title, String content, Boolean isDeleted) {
-        publicTemplateTitle = title;
-        publicTemplateContent = content;
-        this.isDeleted = isDeleted;
+    /**
+     * 공개 템플릿 생성자
+     * 
+     * @param publicTemplateTitle 템플릿 제목
+     * @param publicTemplateContent 템플릿 내용
+     * @param buttonTitle 버튼 제목 (선택사항)
+     */
+    @Builder
+    public PublicTemplate(String publicTemplateTitle, String publicTemplateContent, String buttonTitle) {
+        this.publicTemplateTitle = publicTemplateTitle;
+        this.publicTemplateContent = publicTemplateContent;
+        this.buttonTitle = buttonTitle;
+        this.viewCount = 0;
+        this.shareCount = 0;
+        this.isDeleted = false;
+    }
+    
+    /**
+     * 조회수를 1 증가시킨다.
+     * 템플릿이 조회될 때마다 호출되어야 한다.
+     */
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+    
+    /**
+     * 공유수를 1 증가시킨다.
+     * 템플릿이 '가져오기'될 때마다 호출되어야 한다.
+     */
+    public void increaseShareCount() {
+        this.shareCount++;
     }
 }
