75118df335176d7b9bc838b4006b778c8c901466
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-05 12:45:06 +0900

    feat(repository): 개인 템플릿 즐겨찾기 생성을 위한 repository 인터페이스
    - FavoriteRepository.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/repository/FavoriteRepository.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/repository/FavoriteRepository.java
@@ -1,8 +1,22 @@
 package com.jober.final2teamdrhong.repository;
 
 import com.jober.final2teamdrhong.entity.Favorite;
+import com.jober.final2teamdrhong.entity.IndividualTemplate;
+import com.jober.final2teamdrhong.entity.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {}
+public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
+
+    /**
+     * 특정 워크스페이스와 개인 템플릿으로 즐겨찾기 정보를 조회합니다.
+     * @param workspace 조회할 워크스페이스 엔티티
+     * @param individualTemplate 조회할 개인 템플릿 엔티티
+     * @return 존재하면 Optional<Favorite>, 존재하지 않으면 Optional.empty()
+     */
+    Optional<Favorite> findByWorkspaceAndIndividualTemplate(Workspace workspace, IndividualTemplate individualTemplate);
+
+}
