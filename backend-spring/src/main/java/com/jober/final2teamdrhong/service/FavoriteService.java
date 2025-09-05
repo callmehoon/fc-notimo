e086cf03e75602e4bbd06a3e1fff40fdcd879863
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-05 12:45:55 +0900

    feat(service): 개인 템플릿 즐겨찾기 생성을 위한 service 클래스
    - FavoriteService.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/service/FavoriteService.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/service/FavoriteService.java
@@ -1,4 +1,43 @@
 package com.jober.final2teamdrhong.service;
 
+import com.jober.final2teamdrhong.dto.IndividualTemplateFavoriteRequest;
+import com.jober.final2teamdrhong.entity.Favorite;
+import com.jober.final2teamdrhong.entity.IndividualTemplate;
+import com.jober.final2teamdrhong.entity.Workspace;
+import com.jober.final2teamdrhong.repository.FavoriteRepository;
+import com.jober.final2teamdrhong.repository.IndividualTemplateRepository;
+import com.jober.final2teamdrhong.repository.WorkspaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final WorkspaceRepository workspaceRepository;
+    private final IndividualTemplateRepository individualTemplateRepository;
+
+    /**
+     * 개인 템플릿을 즐겨찾기에 추가(create)
+     * @param request 즐겨찾기 생성을 위한 정보 (workspaceId, templateId)
+     * @throws IllegalArgumentException 워크스페이스, 템플릿이 존재하지 않거나 이미 즐겨찾기로 등록되었을 경우 발생
+     */
+    @Transactional
+    public void createIndividualTemplateFavorite(IndividualTemplateFavoriteRequest request) {
+        Workspace workspace = workspaceRepository.findById(request.getWorkspaceId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 워크스페이스를 찾을 수 없습니다."));
+
+        IndividualTemplate individualTemplate = individualTemplateRepository.findById(request.getIndividualTemplateId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 템플릿을 찾을 수 없습니다."));
+
+        favoriteRepository.findByWorkspaceAndIndividualTemplate(workspace, individualTemplate)
+                .ifPresent(f -> {throw new IllegalArgumentException("이미 즐겨찾기된 템플릿입니다.");});
+
+        Favorite favorite = new Favorite(workspace, null, individualTemplate);
+        favoriteRepository.save(favorite);
+    }
+
 }
