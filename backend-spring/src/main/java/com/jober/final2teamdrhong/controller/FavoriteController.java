596e149a76491d9272eaf38421a8091e88c688e0
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-05 12:46:23 +0900

    feat(controller): 개인 템플릿 즐겨찾기 생성을 위한 controller 클래스
    - FavoriteController.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/controller/FavoriteController.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/controller/FavoriteController.java
@@ -1,4 +1,30 @@
 package com.jober.final2teamdrhong.controller;
 
+import com.jober.final2teamdrhong.dto.IndividualTemplateFavoriteRequest;
+import com.jober.final2teamdrhong.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    /**
+     * 개인 템플릿을 즐겨찾기에 추가(create)
+     * @param request workspaceId와 templateId를 포함한 DTO
+     * @return 성공 시 HTTP 200 OK
+     */
+    @PostMapping("/fav")
+    public ResponseEntity<Void> createTemplateFavorite(@RequestBody IndividualTemplateFavoriteRequest request) {
+        favoriteService.createIndividualTemplateFavorite(request);
+        return ResponseEntity.ok().build();
+    }
+
 }
