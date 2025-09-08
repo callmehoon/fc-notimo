808d674d69af886633dfc2d94a4800bc51de49ca
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-08 15:38:41 +0900

    refactor(dto): dto 계층에서는 모든 필드를 클라이언트 요청으로 처리하므로 전체 필드를 사용하는 생성자를 @AllArgsConstructor로 대체
    - IndividualTemplateFavoriteRequest.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/IndividualTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/IndividualTemplateFavoriteRequest.java
@@ -1,16 +1,13 @@
 package com.jober.final2teamdrhong.dto.favorite;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class IndividualTemplateFavoriteRequest {
     private Integer workspaceId;
     private Integer individualTemplateId;
-
-    public IndividualTemplateFavoriteRequest(Integer workspaceId, Integer individualTemplateId) {
-        this.workspaceId = workspaceId;
-        this.individualTemplateId = individualTemplateId;
-    }
 }
