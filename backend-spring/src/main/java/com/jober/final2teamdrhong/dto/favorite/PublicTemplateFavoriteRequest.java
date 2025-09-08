386d5cc5d51b3a1835adb8ad1d70d2ad52548121
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-08 15:41:26 +0900

    feat(dto): 공용 템플릿 즐겨찾기 생성을 위한 dto 클래스
    - PublicTemplateFavoriteRequest.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
@@ -1,9 +1,13 @@
 package com.jober.final2teamdrhong.dto.favorite;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class PublicTemplateFavoriteRequest  {
+@AllArgsConstructor
+public class PublicTemplateFavoriteRequest {
+    private Integer workspaceId;
+    private Integer publicTemplateId;
 }
