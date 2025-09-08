acf4473a677308aab7f83fc1496cc1490243ed6d
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-08 18:07:54 +0900

    refactor(dto): dto 필드 Not Null 검증 추가
    - IndividualTemplateFavoriteRequest.java
    - PublicTemplateFavoriteRequest.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
@@ -3,11 +3,15 @@ package com.jober.final2teamdrhong.dto.favorite;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PublicTemplateFavoriteRequest {
+    @NotNull(message = "워크스페이스 ID는 필수 입력값입니다.")
     private Integer workspaceId;
+
+    @NotNull(message = "공용 템플릿 ID는 필수 입력값입니다.")
     private Integer publicTemplateId;
 }
