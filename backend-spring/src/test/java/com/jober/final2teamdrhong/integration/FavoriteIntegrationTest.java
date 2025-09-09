6e83d4033b1cb82f5a2e3bdf1a4d1a48c955b1e5
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-09 14:56:53 +0900

    test(controller): 개인/공용 템플릿 즐겨찾기 추가(create) 기능 통합 테스트
    - FavoriteIntegrationTest.java

--- backend-spring/src/test/java/com/jober/final2teamdrhong/integration/FavoriteIntegrationTest.java
+++ backend-spring/src/test/java/com/jober/final2teamdrhong/integration/FavoriteIntegrationTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
-class FavoriteIntegrationTest {
+class FavoriteCreationIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
