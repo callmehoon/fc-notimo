4b0f71038c348bf31074cccfb7505f2b329530a4
bill291104 <bill29123@gmail.com>
2025-09-11 17:12:48 +0900

    fix(fastapi): import 경로 문제 수정
    이제 main.py 를 실행하면
    FastAPI 서버가 구동됩니다

--- backend-ai/app/schemas/template.py
+++ backend-ai/app/schemas/template.py
@@ -1,7 +1,9 @@
-class TemplateRequest:
+from pydantic import BaseModel
+
+class TemplateRequest(BaseModel):
     # TODO 템플릿 생성 요청 객체 정의
     pass
 
-class TemplateResponse:
+class TemplateResponse(BaseModel):
     # TODO 템플릿 생성 응답 객체 정의
     pass
