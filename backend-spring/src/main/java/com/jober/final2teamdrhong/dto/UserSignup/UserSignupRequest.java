f06e011c1f33805a64c06083c0c080661c94008a
sangmin <mfits603@gmail.com>
2025-09-12 15:55:02 +0900

    fix(EmailRequest , User Dto): DTO 컨벤션에 맞춤

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/UserSignup/UserSignupRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/UserSignup/UserSignupRequest.java
@@ -1,4 +1,4 @@
-package com.jober.final2teamdrhong.dto;
+package com.jober.final2teamdrhong.dto.UserSignup;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
