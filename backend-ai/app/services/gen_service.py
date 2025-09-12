d82afe64f38d1f4addb9558763e5990b6aa5fdf1
bill291104 <bill29123@gmail.com>
2025-09-12 17:43:50 +0900

    fix(import): 새로 만든 모델 로더를 사용하도록 import

--- backend-ai/app/services/gen_service.py
+++ backend-ai/app/services/gen_service.py
@@ -1,8 +1,8 @@
-from ..core.model_loader import models
 from ..schemas.template import TemplateRequest, TemplateResponse
+from ..core.model_loader import model_loader
 
 def gen_template(request: TemplateRequest) -> TemplateResponse:
     # TODO 템플릿 생성 함수 구현
     pass
-    gen_model = models.get("gen_model")
+    gen_model, gen_tokenizer = model_loader.models.get("gen_model")
     return TemplateResponse()
