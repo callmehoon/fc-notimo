bc15753bf9519071a060a6e01aeb1e049d3dc914
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-12 10:26:57 +0900

    test(controller): 조회기능 테스트 추가
    - FavoriteControllerTest.java

--- backend-spring/src/test/java/com/jober/final2teamdrhong/controller/FavoriteControllerTest.java
+++ backend-spring/src/test/java/com/jober/final2teamdrhong/controller/FavoriteControllerTest.java
@@ -27,9 +27,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FavoriteController.class)
