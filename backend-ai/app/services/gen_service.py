dae1d45cfe1822b1ce5b669646d266f333056ab9
bill291104 <bill29123@gmail.com>
2025-09-16 17:48:49 +0900

    refactor(gen-service): 함수 책임 분리
    - LangChain 적용을 고려하여 개발용 로컬 서버에서도 생성이 동작하도록 구조 변경

--- backend-ai/app/services/gen_service.py
+++ backend-ai/app/services/gen_service.py
@@ -1,27 +1,31 @@
 from ..schemas.template import TemplateRequest, TemplateResponse
 from ..core.model_loader import model_loader
+from typing import List
 
 def gen_template(request: TemplateRequest) -> TemplateResponse:
     models = model_loader.models
     gen_model, gen_tokenizer = models.get("gen")
     if gen_model == "dummy_gen_model" or gen_tokenizer == "dummy_gen_tokenizer":
-        return generate_dummy_template(request)
+        template = generate_dummy_template(request.user_input, request.related_policy)
     else:
-        return generate_template(request)
+        template = generate_template(request.user_input, request.related_policy)
 
-def generate_dummy_template(request: TemplateRequest) -> TemplateResponse:
+    # TODO LangChain 적용
     return TemplateResponse(
-        template="{\"title\": \"회사소개서 발송\", \"text\": \"안녕하세요 #{수신자명}님,\n\n#{회사명}은 #{업종} 분야에서 활동하는 #{회사명}입니다.\n\n▶ 회사명 : #{회사명}\n▶ 업종 : #{업종}\n▶ 연락처 : #{연락처}\n\n감사합니다.\", \"button_name\": \"자세히 보기\"}",
-        chat_response="개발용 로컬 서버의 데모입니다. GPU 가 탑제된 추론서버로 배포하면 정상적으로 기능이 동작할 것입니다."
+        template=template, # TODO LangChain 의 Agent 에게 parsing tool 을 제공
+        chat_response="None" # TODO LangChain 의 Agent 로 chat_response 생성
     )
 
-def generate_template(request: TemplateRequest) -> TemplateResponse:
+def generate_dummy_template(user_input: str, related_policy: List[str]) -> str:
+    return '{"title": "가짜 템플릿", "text": "GPU 가 없는 개발용 로컬 환경에서 반환되는 결과입니다.\n변수들을 테스트 해보세요.\n#{변수1}, #{변수2}\n감사합니다.", "button_name": "버튼 이름"}'
+
+def generate_template(user_input: str, related_policy: List[str]) -> str:
     import torch
 
     models = model_loader.models
     gen_model, gen_tokenizer = models.get("gen")
 
-    prompt = f"user_input: {request.user_input}\npolicy: {request.related_policy}\ntemplate: "
+    prompt = f"user_input: {user_input}\npolicy: {related_policy}\ntemplate: "
 
     inputs = gen_tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True).to(gen_model.device)
 
@@ -36,11 +40,4 @@ def generate_template(request: TemplateRequest) -> TemplateResponse:
         )
 
     response = gen_tokenizer.decode(outputs[0], skip_special_tokens=True)
-    template_txt = response.split("template: ")[-1].strip()
-
-    # TODO LangChain Agent 사용해서 template parsing & chat_response 생성
-
-    return TemplateResponse(
-        template = template_txt,
-        chat_response = "None"
-    )
+    return response.split("template: ")[-1].strip()
