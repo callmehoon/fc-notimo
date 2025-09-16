a3535354b80a8a78965f89ab2632fe59efccc18d
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-16 16:30:36 +0900

    refactor(dto): Jackson 바인더의 리플렉션 API를 고려하여 @NoArgsConstructor 삭제
    - FavoritePageRequest.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/IndividualTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/IndividualTemplateFavoriteRequest.java
@@ -3,10 +3,8 @@ package com.jober.final2teamdrhong.dto.favorite;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class IndividualTemplateFavoriteRequest {
     @NotNull(message = "워크스페이스 ID는 필수 입력값입니다.")
@@ -14,9 +12,4 @@ public class IndividualTemplateFavoriteRequest {
 
     @NotNull(message = "개인 템플릿 ID는 필수 입력값입니다.")
     private Integer individualTemplateId;
-
-    public IndividualTemplateFavoriteRequest(Integer workspaceId, Integer individualTemplateId) {
-        this.workspaceId = workspaceId;
-        this.individualTemplateId = individualTemplateId;
-    }
 }
--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
@@ -1,12 +1,10 @@
 package com.jober.final2teamdrhong.dto.favorite;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotNull;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class PublicTemplateFavoriteRequest {
     @NotNull(message = "워크스페이스 ID는 필수 입력값입니다.")
