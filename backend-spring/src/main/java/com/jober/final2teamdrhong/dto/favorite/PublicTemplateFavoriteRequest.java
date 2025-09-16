bc842bdb52ea08b738df1571191a82953257bed6
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-16 16:47:50 +0900

    refactor(dto): 피드백 반영하여 @NoArgsConstructor 어노테이션 삭제.
    - IndividualTemplateFavoriteRequest.java
    - PublicTemplateFavoriteRequest.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/favorite/PublicTemplateFavoriteRequest.java
@@ -2,11 +2,9 @@ package com.jober.final2teamdrhong.dto.favorite;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotNull;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class PublicTemplateFavoriteRequest {
     @NotNull(message = "워크스페이스 ID는 필수 입력값입니다.")
