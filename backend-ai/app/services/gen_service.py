5f49c79ba7ab6d7b49df745064a22ac58f142280
bill291104 <bill29123@gmail.com>
2025-09-16 16:36:35 +0900

    feat(gen-service): 더미 생성과 실제 생성 함수 모두 정의
    - 더미 생성 함수는 GPU 가 없는 로컬 개발용 서버에서
    - 실제 생성 함수는 GPU 가 있는 배포서버에서

--- backend-ai/app/services/gen_service.py
+++ backend-ai/app/services/gen_service.py
@@ -2,7 +2,45 @@ from ..schemas.template import TemplateRequest, TemplateResponse
 from ..core.model_loader import model_loader
 
 def gen_template(request: TemplateRequest) -> TemplateResponse:
-    # TODO 템플릿 생성 함수 구현
-    pass
-    gen_model, gen_tokenizer = model_loader.models.get("gen")
-    return TemplateResponse()
+    models = model_loader.models
+    gen_model, gen_tokenizer = models.get("gen")
+    if gen_model == "dummy_gen_model" or gen_tokenizer == "dummy_gen_tokenizer":
+        return generate_dummy_template(request)
+    else:
+        return generate_template(request)
+
+def generate_dummy_template(request: TemplateRequest) -> TemplateResponse:
+    return TemplateResponse(
+        template="{\"title\": \"회사소개서 발송\", \"text\": \"안녕하세요 #{수신자명}님,\n\n#{회사명}은 #{업종} 분야에서 활동하는 #{회사명}입니다.\n\n▶ 회사명 : #{회사명}\n▶ 업종 : #{업종}\n▶ 연락처 : #{연락처}\n\n감사합니다.\", \"button_name\": \"자세히 보기\"}",
+        chat_response="개발용 로컬 서버의 데모입니다. GPU 가 탑제된 추론서버로 배포하면 정상적으로 기능이 동작할 것입니다."
+    )
+
+def generate_template(request: TemplateRequest) -> TemplateResponse:
+    import torch
+
+    models = model_loader.models
+    gen_model, gen_tokenizer = models.get("gen")
+
+    prompt = f"user_input: {request.user_input}\npolicy: {request.related_policy}\ntemplate: "
+
+    inputs = gen_tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True).to(gen_model.device)
+
+    with torch.no_grad():
+        outputs = gen_model.generate(
+            **inputs,
+            max_new_tokens=150,
+            temperature=0.3,
+            do_sample=True,
+            pad_token_id=gen_tokenizer.eos_token_id,
+            early_stopping=True
+        )
+
+    response = gen_tokenizer.decode(outputs[0], skip_special_tokens=True)
+    template_txt = response.split("template: ")[-1].strip()
+
+    # TODO LangChain Agent 사용해서 template parsing & chat_response 생성
+
+    return TemplateResponse(
+        template = template_txt,
+        chat_response = "None"
+    )
