e173251e20d0335895d6a2e2123d563840f07335
bill291104 <bill29123@gmail.com>
2025-09-17 19:00:32 +0900

    feat(gen-service): 에이전트 기능 추가

--- backend-ai/app/services/gen_service.py
+++ backend-ai/app/services/gen_service.py
@@ -1,43 +1,25 @@
-from ..schemas.template import TemplateRequest, TemplateResponse
-from ..core.model_loader import model_loader
-from typing import List
+from langchain.agents.output_parsers.tools import ToolAgentAction
 
-def gen_template(request: TemplateRequest) -> TemplateResponse:
-    models = model_loader.models
-    gen_model, gen_tokenizer = models.get("gen")
-    if gen_model == "dummy_gen_model" or gen_tokenizer == "dummy_gen_tokenizer":
-        template = generate_dummy_template(request.user_input, request.related_policy)
-    else:
-        template = generate_template(request.user_input, request.related_policy)
+from ..schemas.template import TemplateRequest, TemplateResponse, Template
+from ..core.agent_initializer import AgentInitializer
 
-    # TODO LangChain 적용
+async def gen_template(request: TemplateRequest) -> TemplateResponse:
+    agent_executor = AgentInitializer.agent_executor
+
+    result = await agent_executor.ainvoke({
+        "original_template": request.original_template.model_dump_json(indent=2),
+        "user_input": request.user_input,
+        "related_policy": request.related_policy,
+    })
+
+    agent_output = None
+
+    for action, observation in reversed(result.get('intermediate_steps', [])):
+        if isinstance(action, ToolAgentAction) and action.tool == 'create_final_output':
+            agent_output = observation
+            break
+
     return TemplateResponse(
-        template=template, # TODO LangChain 의 Agent 에게 parsing tool 을 제공
-        chat_response="None" # TODO LangChain 의 Agent 로 chat_response 생성
+        template=agent_output.template,
+        chat_response=agent_output.chat_message
     )
-
-def generate_dummy_template(user_input: str, related_policy: List[str]) -> str:
-    return '{"title": "가짜 템플릿", "text": "GPU 가 없는 개발용 로컬 환경에서 반환되는 결과입니다.\n변수들을 테스트 해보세요.\n#{변수1}, #{변수2}\n감사합니다.", "button_name": "버튼 이름"}'
-
-def generate_template(user_input: str, related_policy: List[str]) -> str:
-    import torch
-
-    models = model_loader.models
-    gen_model, gen_tokenizer = models.get("gen")
-
-    prompt = f"user_input: {user_input}\npolicy: {related_policy}\ntemplate: "
-
-    inputs = gen_tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True).to(gen_model.device)
-
-    with torch.no_grad():
-        outputs = gen_model.generate(
-            **inputs,
-            max_new_tokens=150,
-            temperature=0.3,
-            do_sample=True,
-            pad_token_id=gen_tokenizer.eos_token_id,
-            early_stopping=True
-        )
-
-    response = gen_tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response.split("template: ")[-1].strip()
