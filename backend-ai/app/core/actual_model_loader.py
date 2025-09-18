050da74652d74c156480d41968dfea4c58f06e77
bill291104 <bill29123@gmail.com>
2025-09-18 10:38:11 +0900

    feat(model-loader): 로깅으로 변경

--- backend-ai/app/core/actual_model_loader.py
+++ backend-ai/app/core/actual_model_loader.py
@@ -1,21 +1,59 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, AutoModelForSequenceClassification
 from peft import PeftModel
 import torch
+from huggingface_hub import snapshot_download
+from huggingface_hub.utils import RepositoryNotFoundError
 import gc
 
+import logging
+log = logging.getLogger("uvicorn")
+
 class ModelLoader:
-    GEN_MODEL_PATH = "MLP-KTLim/llama-3-Korean-Bllossom-8B".replace("/", "--")
-    CLS_MODEL_PATH = "klue/bert-base".replace("/", "--")
+    GEN_MODEL_ID = "MLP-KTLim/llama-3-Korean-Bllossom-8B"
+    CLS_MODEL_ID = "klue/bert-base"
+    GEN_MODEL_PATH = GEN_MODEL_ID.replace("/", "--")
+    CLS_MODEL_PATH = CLS_MODEL_ID.replace("/", "--")
     DOWNLOADED_PATH = "../models/downloaded_model/"
     FINETUNED_PATH = "../models/finetuned_model/"
 
     models = {}
 
     @classmethod
+    def download_gen_model(cls):
+        cls.download_model_from_hub(cls.GEN_MODEL_ID, cls.DOWNLOADED_PATH + cls.GEN_MODEL_PATH)
+        return
+
+    @classmethod
+    def download_cls_model(cls):
+        cls.download_model_from_hub(cls.CLS_MODEL_ID, cls.DOWNLOADED_PATH + cls.CLS_MODEL_PATH)
+        return
+
+    @classmethod
+    def download_model_from_hub(cls, model_id, local_dir):
+        log.info(f"'{model_id}' 모델을 '{local_dir}' 경로에 다운로드합니다...")
+        try:
+            # snapshot_download는 알아서 기존 파일을 체크하고 필요한 것만 다운로드합니다.
+            model_path = snapshot_download(
+                repo_id=model_id,
+                local_dir=local_dir
+                # resume_download=True, # 기본값이 True이므로 명시하지 않아도 됨
+            )
+            log.info("✅ 모델 준비 완료!")
+            return model_path
+        except RepositoryNotFoundError as e:
+            log.error(f"❌ 오류: 모델 ID '{model_id}'를 찾을 수 없습니다.")
+            raise e
+        except Exception as e:
+            log.error(f"❌ 다운로드 중 오류가 발생했습니다: {e}")
+            raise e
+
+    @classmethod
     def load_gen_model(cls):
         """서버 구동시에 호출할 템플릿 생성 모델을 메모리로 로드하는 함수"""
 
-        if "gen" not in modles or modles["gen"] is None:
+        cls.download_gen_model()
+
+        if "gen" not in cls.models or cls.models["gen"] is None:
             base_model_path = cls.DOWNLOADED_PATH + cls.GEN_MODEL_PATH
             lora_adapter_path = cls.FINETUNED_PATH + cls.GEN_MODEL_PATH
 
@@ -33,12 +71,12 @@ class ModelLoader:
             )
             model = PeftModel.from_pretrained(base_model, lora_adapter_path)
 
-            models["gen"] = model, tokenizer
-            print("생성 모델 로드 완료")
+            cls.models["gen"] = model, tokenizer
+            log.info("생성 모델 로드 완료")
             cls.check_gpu_memory_usage()
             return cls.models
         else:
-            print("생성 모델이 이미 로드 되어 있습니다")
+            log.warning("생성 모델이 이미 로드 되어 있습니다")
             cls.check_gpu_memory_usage()
             return cls.models
 
@@ -47,7 +85,7 @@ class ModelLoader:
         """생성 모델 메모리에서 정리"""
         cls.check_gpu_memory_usage()
 
-        gen_model, gen_tokenizer = models.pop("gen")
+        gen_model, gen_tokenizer = cls.models.pop("gen")
         del gen_model
         del gen_tokenizer
         torch.cuda.empty_cache()
@@ -59,7 +97,10 @@ class ModelLoader:
     @classmethod
     def load_cls_model(cls):
         """서버 구동시에 호출할 템플릿 검증 모델을 메모리로 로드하는 함수"""
-        if "cls" not in modles or modles["cls"] is None:
+
+        cls.download_cls_model()
+
+        if "cls" not in cls.models or cls.models["cls"] is None:
             base_model_path = cls.DOWNLOADED_PATH + cls.CLS_MODEL_PATH
             finetuned_model_path = cls.FINETUNED_PATH + cls.CLS_MODEL_PATH
 
@@ -68,24 +109,24 @@ class ModelLoader:
             model.eval()
 
             cls.models["cls"] = model, tokenizer
-            print("분류 모델 로드 완료")
+            log.info("분류 모델 로드 완료")
             cls.check_gpu_memory_usage()
             return cls.models
         else:
-            print("분류 모델이 이미 로드 되어 있습니다")
+            log.warning("분류 모델이 이미 로드 되어 있습니다")
             return cls.models
     @classmethod
     def unload_cls_model(cls):
         """메모리 정리"""
         cls.check_gpu_memory_usage()
 
-        cls_model, cls_tokenizer = models.pop("cls")
+        cls_model, cls_tokenizer = cls.models.pop("cls")
         del cls_model
         del cls_tokenizer
         torch.cuda.empty_cache()
         gc.collect()
 
-        check_gpu_memory_usage()
+        cls.check_gpu_memory_usage()
         return
 
     @staticmethod
@@ -102,8 +143,7 @@ class ModelLoader:
             allocated_gb = allocated_bytes / gb_factor
             reserved_gb = reserved_bytes / gb_factor
 
-            print(f"현재 사용 중인 GPU 메모리: {allocated_gb:.2f} GB")
-            print(f"현재 캐시된 GPU 메모리: {reserved_gb:.2f} GB")
-            # TODO 로깅으로 변경
+            log.info(f"현재 사용 중인 GPU 메모리: {allocated_gb:.2f} GB")
+            log.info(f"현재 캐시된 GPU 메모리: {reserved_gb:.2f} GB")
         else:
-            print("GPU를 사용할 수 없습니다.")
+            log.error("GPU를 사용할 수 없습니다.")
--- backend-ai/app/core/dummy_model_loader_for_local.py
+++ backend-ai/app/core/dummy_model_loader_for_local.py
@@ -1,10 +1,13 @@
+import logging
+log = logging.getLogger("uvicorn")
+
 class DummyModelLoader:
     models = {}
 
     @classmethod
     def load_gen_model(cls):
         """생성 모델 로드를 모킹하는 함수"""
-        print("더미 생성 모델 로드 완료")
+        log.info("더미 생성 모델 로드 완료")
         dummy_gen_model = "dummy_gen_model"
         dummy_gen_tokenizer = "dummy_gen_tokenizer"
         cls.models["gen"] = dummy_gen_model, dummy_gen_tokenizer
@@ -13,7 +16,7 @@ class DummyModelLoader:
     @classmethod
     def load_cls_model(cls):
         """분류 모델 로드를 모킹하는 함수"""
-        print("더미 분류 모델 로드 완료")
+        log.info("더미 분류 모델 로드 완료")
         dummy_cls_model = "dummy_cls_model"
         dummy_cls_tokenizer = "dummy_cls_tokenizer"
         cls.models["cls"] = dummy_cls_model, dummy_cls_tokenizer
@@ -22,9 +25,9 @@ class DummyModelLoader:
     @classmethod
     def unload_gen_model(cls):
         """생성 모델 정리를 모킹하는 함수"""
-        print("생성 모델 정리")
+        log.info("생성 모델 정리")
 
     @classmethod
     def unload_cls_model(cls):
         """분류 모델 정리를 모킹하는 함수"""
-        print("분류 모델 정리")
+        log.info("분류 모델 정리")
