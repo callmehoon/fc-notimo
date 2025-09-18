370bfc1fb8c58e2bd5bea265ad2b47b51c48e81e
seonghoon <seonghoon315@naver.com>
2025-09-18 11:05:37 +0900

    feat(RecipientValidator): 여러 수신자 ID 일괄 검증 로직 추가
    - RecipientValidator에 다수의 수신자 ID 목록이 특정 워크스페이스에 모두 유효하게 존재하는지 일괄 검증하는 validateAndGetRecipients 메서드를 추가
    - RecipientRepository의 일괄 조회 기능을 사용하여 DB 조회를 최소화
    - 요청된 ID 개수와 DB에서 조회된 엔티티 개수를 비교하여, 유효하지 않은 ID가 하나라도 포함된 경우 IllegalArgumentException을 발생

--- backend-spring/src/main/java/com/jober/final2teamdrhong/service/validator/RecipientValidator.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/service/validator/RecipientValidator.java
@@ -5,6 +5,8 @@ import com.jober.final2teamdrhong.repository.RecipientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class RecipientValidator {
@@ -23,5 +25,22 @@ public class RecipientValidator {
     public Recipient validateAndGetRecipient(Integer workspaceId, Integer recipientId) {
         return recipientRepository.findByRecipientIdAndWorkspace_WorkspaceId(recipientId, workspaceId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 워크스페이스에 존재하지 않는 수신자입니다. ID: " + recipientId));
+    }
+
+    /**
+     * 요청받은 수신자 ID 리스트가 모두 해당 워크스페이스에 존재하는지 검증합니다.
+     * 검증에 성공하면 수신자 엔티티 리스트를 반환하고, 실패하면 예외를 발생시킵니다.
+     *
+     * @param workspaceId 수신자들이 속한 워크스페이스의 ID
+     * @param recipientIds 검증할 수신자들의 ID 리스트
+     * @return 검증에 성공한 Recipient 엔티티 리스트
+     * @throws IllegalArgumentException ID 개수 불일치 또는 존재하지 않는 수신자가 포함된 경우
+     */
+    public List<Recipient> validateAndGetRecipients(Integer workspaceId, List<Integer> recipientIds) {
+        List<Recipient> recipientList = recipientRepository.findAllByWorkspace_WorkspaceIdAndRecipientIdIn(workspaceId, recipientIds);
+        if (recipientList.size() != recipientIds.size()) {
+            throw new IllegalArgumentException("요청된 수신자 목록에 유효하지 않거나 권한이 없는 ID가 포함되어 있습니다.");
+        }
+        return recipientList;
     }
 }
