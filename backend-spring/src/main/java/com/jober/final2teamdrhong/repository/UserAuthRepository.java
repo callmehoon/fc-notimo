05138d9195ed05d759f5c67d53459f31819048f4
sangmin <mfits603@gmail.com>
2025-09-19 17:30:30 +0900

    refactor(UserAuthRepository): 불필요한 import 제거

--- backend-spring/src/main/java/com/jober/final2teamdrhong/repository/UserAuthRepository.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/repository/UserAuthRepository.java
@@ -4,6 +4,5 @@ import com.jober.final2teamdrhong.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserAuthRepository extends JpaRepository<UserAuth, Integer> {
 }
