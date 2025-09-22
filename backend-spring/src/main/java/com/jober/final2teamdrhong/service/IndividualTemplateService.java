d14b6ea368fffc4f1a44224b0d3fbe8c8218f63c
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-22 10:25:24 +0900

    refactor(service): WorkspaceRepository 인터페이스의 findByIdOrThrow default 메서드에서 userId 추가로 인한 수정
    - IndividualTemplateService.java

--- backend-spring/src/main/java/com/jober/final2teamdrhong/service/IndividualTemplateService.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/service/IndividualTemplateService.java
@@ -75,13 +75,14 @@ public class IndividualTemplateService {
     @Transactional
     public IndividualTemplateResponse createIndividualTemplateFromPublic(
             Integer publicTemplateId,
-            Integer workspaceId
+            Integer workspaceId,
+            Integer userId
     ) {
         // 공용 템플릿 조회
         PublicTemplate publicTemplate = publicTemplateRepository.findByIdOrThrow(publicTemplateId);
 
         // 워크스페이스 조회
-        Workspace workspace = workspaceRepository.findByIdOrThrow(workspaceId);
+        Workspace workspace = workspaceRepository.findByIdOrThrow(workspaceId, userId);
 
         // 복사 후 개인 템플릿 생성
         IndividualTemplate newIndividualTemplate = IndividualTemplate.builder()
@@ -100,13 +101,14 @@ public class IndividualTemplateService {
     @Transactional
     public CompletableFuture<IndividualTemplateResponse> createIndividualTemplateFromPublicAsync(
             Integer publicTemplateId,
-            Integer workspaceId
+            Integer workspaceId,
+            Integer userId
     ) {
         boolean isVirtual = Thread.currentThread().isVirtual();
         log.info("[@Async] thread={}, isVirtual={}", Thread.currentThread().getName(), isVirtual);
 
         IndividualTemplateResponse individualTemplateResponse = createIndividualTemplateFromPublic(
-                publicTemplateId, workspaceId);
+                publicTemplateId, workspaceId, userId);
         return CompletableFuture.completedFuture(individualTemplateResponse);
     }
 }
