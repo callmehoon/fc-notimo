9496b3eb477081ed175b65b6cc62237239aed805
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-22 14:38:36 +0900

    test(controller): 즐겨찾기 삭제 통합 테스트 추가
    - FavoriteIntegrationTest.java

--- backend-spring/src/test/java/com/jober/final2teamdrhong/integration/FavoriteIntegrationTest.java
+++ backend-spring/src/test/java/com/jober/final2teamdrhong/integration/FavoriteIntegrationTest.java
@@ -18,7 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
