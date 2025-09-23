576f8d06e86a5ca2b6e367dd9715783067ed49c9
seonghoon <seonghoon315@naver.com>
2025-09-23 11:14:29 +0900

    feat(RecipientValidator): 수신자 중복 검증 로직을 Validator로 분리
    - RecipientService에 있던 수신자 중복 생성 검증 로직을 RecipientValidator로 이동하여 역할과 책임을 명확히 분리

--- backend-spring/src/main/java/com/jober/final2teamdrhong/service/validator/RecipientValidator.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/service/validator/RecipientValidator.java
@@ -1,6 +1,7 @@
 package com.jober.final2teamdrhong.service.validator;
 
 import com.jober.final2teamdrhong.entity.Recipient;
+import com.jober.final2teamdrhong.entity.Workspace;
 import com.jober.final2teamdrhong.repository.RecipientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,21 @@ import org.springframework.stereotype.Component;
 public class RecipientValidator {
 
     private final RecipientRepository recipientRepository;
+
+    /**
+     * 특정 워크스페이스에 동일한 이름과 전화번호를 가진 수신자가 이미 존재하는지 검증합니다.
+     * 중복이 발견되면 예외를 발생시킵니다.
+     *
+     * @param workspace            검사를 수행할 워크스페이스 엔티티
+     * @param recipientName        중복 여부를 확인할 수신자 이름
+     * @param recipientPhoneNumber 중복 여부를 확인할 수신자 전화번호
+     * @throws IllegalArgumentException 동일한 이름과 번호의 수신자가 이미 존재할 경우
+     */
+    public void validateNoDuplicateRecipientExists(Workspace workspace, String recipientName, String recipientPhoneNumber) {
+        if (recipientRepository.existsByWorkspaceAndRecipientNameAndRecipientPhoneNumber(workspace, recipientName, recipientPhoneNumber)) {
+            throw new IllegalArgumentException("해당 워크스페이스에 동일한 이름과 번호의 수신자가 이미 존재합니다.");
+        }
+    }
 
     /**
      * 특정 워크스페이스에 해당 수신자가 존재하는지 검증합니다.
