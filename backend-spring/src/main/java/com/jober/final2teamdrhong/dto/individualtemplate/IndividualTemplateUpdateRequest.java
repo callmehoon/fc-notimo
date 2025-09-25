59bd9813da136d406b742990bf309e0c000e8cb0
constant0841 <dltkdtn0841@naver.com>
2025-09-25 16:15:13 +0900

    refactor: IndividualTemplateUpdateRequest 불필요한 NoArgsConstructor 제거
    - @NoArgsConstructor 제거하여 불필요한 생성자 제거
    - DTO는 요청 매핑 시 필요한 생성자와 세터만 유지
    - 불변성 및 코드 간결성 강화

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/individualtemplate/IndividualTemplateUpdateRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/individualtemplate/IndividualTemplateUpdateRequest.java
@@ -2,10 +2,8 @@ package com.jober.final2teamdrhong.dto.individualtemplate;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class IndividualTemplateUpdateRequest {
     private String individualTemplateTitle;
