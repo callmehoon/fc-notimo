139d0833cbb353afd6452eeeb51b7f60c2129a56
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-25 16:35:43 +0900

    feat(ChatSessionRepository.java): 채팅방ID, 워크스페이스ID, 사용자ID를 조회하여 조회 권한 검증하는 메서드 추가

--- backend-spring/src/main/java/com/jober/final2teamdrhong/repository/ChatSessionRepository.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/repository/ChatSessionRepository.java
@@ -4,5 +4,9 @@ import com.jober.final2teamdrhong.entity.ChatSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ChatSessionRepository extends JpaRepository<ChatSession, Integer> {}
+public interface ChatSessionRepository extends JpaRepository<ChatSession, Integer> {
+    Optional<ChatSession> findBySessionIdAndWorkspace_WorkspaceIdAndWorkspace_User_UserId(Integer sessionId, Integer workspaceId, Integer userId);
+}
