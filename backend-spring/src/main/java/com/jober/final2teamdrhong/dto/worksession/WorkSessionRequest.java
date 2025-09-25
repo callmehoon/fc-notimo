234d6d9a4415e640ed6990da74147f3e0883c8b0
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-25 21:33:53 +0900

    fix(/dto/worksession): 기존 작업세션 기능 변경으로 코드 삭제

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/worksession/WorkSessionRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/worksession/WorkSessionRequest.java
@@ -1,12 +1,4 @@
 package com.jober.final2teamdrhong.dto.worksession;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.NotBlank;
-
 public class WorkSessionRequest {
 }
--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/worksession/WorkSessionResponse.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/worksession/WorkSessionResponse.java
@@ -1,10 +1,4 @@
 package com.jober.final2teamdrhong.dto.worksession;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-
 public class WorkSessionResponse {
 }
