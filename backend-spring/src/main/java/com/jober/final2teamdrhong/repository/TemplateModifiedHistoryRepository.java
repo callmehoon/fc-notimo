d701f0c203cfaa3286eb3caa1eb58ea0fcf84bec
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-26 14:34:39 +0900

    feat(TemplateModifiedHistoryRepository): 템플릿 이력 조회 Repository

--- backend-spring/src/main/java/com/jober/final2teamdrhong/repository/TemplateModifiedHistoryRepository.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/repository/TemplateModifiedHistoryRepository.java
@@ -1,8 +1,18 @@
 package com.jober.final2teamdrhong.repository;
 
+import com.jober.final2teamdrhong.entity.IndividualTemplate;
 import com.jober.final2teamdrhong.entity.TemplateModifiedHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TemplateModifiedHistoryRepository extends JpaRepository<TemplateModifiedHistory, Integer> {}
+public interface TemplateModifiedHistoryRepository extends JpaRepository<TemplateModifiedHistory, Integer> {
+    /**
+     * 특정 개인 템플릿에 속한 모든 수정 이력을 생성일자 최신순으로 조회합니다.
+     * @param individualTemplate 조회할 개인 템플릿 엔티티
+     * @return TemplateModifiedHistory 엔티티 리스트
+     */
+    List<TemplateModifiedHistory> findAllByIndividualTemplateOrderByCreatedAtDesc(IndividualTemplate individualTemplate);
+}
