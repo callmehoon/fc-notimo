f758574502fd97a19b0cb6dea9e6cfcd1777deeb
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-09-26 18:51:30 +0900

    fix(IndividualTemplateUpdateRequest): @NotBlank 추가

--- backend-spring/src/main/java/com/jober/final2teamdrhong/dto/individualtemplate/IndividualTemplateUpdateRequest.java
+++ backend-spring/src/main/java/com/jober/final2teamdrhong/dto/individualtemplate/IndividualTemplateUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.jober.final2teamdrhong.dto.individualtemplate;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,9 @@ public class IndividualTemplateUpdateRequest {
     private String individualTemplateTitle;
     private String individualTemplateContent;
     private String buttonTitle;
+
+    @NotBlank
     private String chatAi;
+    @NotBlank
     private String chatUser;
 }
